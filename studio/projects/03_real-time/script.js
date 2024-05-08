9f430b904716c5570dfa6f20649347da1a969bc9
lavanyaranjan <ranjl416@newschool.edu>
2024-05-07 20:10:36 -0400

    js api https

--- studio/projects/03_real-time/script.js
+++ studio/projects/03_real-time/script.js
@@ -1,6 +1,6 @@
 // Function to fetch data from the API
 async function fetchAirPollutionData(lat, lon, apiKey) {
-  const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
   try {
     const response = await fetch(url);
